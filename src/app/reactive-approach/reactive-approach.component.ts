5992336628a9d4e334fb62f11df41e3db4624e71
MadanMohanReddy <madan.patakota@hotmail.com>
2022-08-24 08:23:01 +0530

    Commit 24 August

--- src/app/reactive-approach/reactive-approach.component.ts
+++ src/app/reactive-approach/reactive-approach.component.ts
@@ -4,26 +4,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive-approach',
   templateUrl: './reactive-approach.component.html',
-  styleUrls: ['./reactive-approach.component.css']
+  styleUrls: ['./reactive-approach.component.css'],
 })
 export class ReactiveApproachComponent implements OnInit {
-
-  constructor() { }
-  companyFormGroup : FormGroup;
+  constructor() {}
+  companyFormGroup: FormGroup;
   ngOnInit(): void {
-        this.companyFormGroup = new FormGroup({
-             'companyEmail' : new FormControl(null,
-              [ 
-                Validators.required,
-                Validators.minLength(5),
-                
-              ])
-        })
+    this.companyFormGroup = new FormGroup({
+      companyEmail: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.email,
+      ]),
+      cityName:new FormControl(
+        null,[Validators.required]),
+      security: new FormGroup({
+        securityAnswer : new FormControl(null,[Validators.required])
+      })
+    });
   }
 
+  cityNames = [
+    {
+      ID: 1,
+      name: 'Bangolore',
+    },
+    {
+      ID: 2,
+      name: 'Hyderabad',
+    },
+    {
+      ID: 3,
+      name: 'Chennai',
+    }
+  ];
 
-  evtSubmit(){
-      console.log(this.companyFormGroup);
+  evtSubmit() {
+    console.log(this.companyFormGroup);
   }
-
 }
