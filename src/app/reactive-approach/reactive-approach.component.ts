abf9fac6b4cca1301f7d18bdc6a826b8829b5abf
MadanMohanReddy <madan.patakota@hotmail.com>
2022-08-25 08:55:48 +0530

    Recative FormArray and Custom Validagtion

--- src/app/reactive-approach/reactive-approach.component.ts
+++ src/app/reactive-approach/reactive-approach.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-approach',
@@ -9,19 +9,80 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveApproachComponent implements OnInit {
   constructor() {}
   companyFormGroup: FormGroup;
+
+  CustomerDetails: FormArray;
+  CustomerForm: FormGroup;
+
+
+  CustomValidationForm : FormGroup;
   ngOnInit(): void {
+
     this.companyFormGroup = new FormGroup({
       companyEmail: new FormControl(null, [
         Validators.required,
         Validators.minLength(5),
         Validators.email,
       ]),
-      cityName:new FormControl(
-        null,[Validators.required]),
+      cityName: new FormControl(null, [Validators.required]),
       security: new FormGroup({
-        securityAnswer : new FormControl(null,[Validators.required])
-      })
+        securityAnswer: new FormControl(null, [Validators.required]),
+      }),
     });
+
+    this.CustomerForm = new FormGroup({
+      CustomerDetails: new FormArray([
+        new FormGroup({
+          ContactName: new FormControl(),
+          OrderName: new FormControl(),
+        }),
+      ]),
+    });
+    console.log(this.CustomerForm);
+
+
+    this.CustomValidationForm = new FormGroup({
+       'validEmail' : new FormControl(null,[
+        Validators.required ,
+         this.gmailDomain])
+    })
+
+
+  }
+
+
+  name : string = "";
+
+  // {'isnotGmail' : true};
+
+  gmailDomain(control:FormControl) : { [key:string] : boolean } | null{
+    let emailValue = control.value;
+    let returnany : any;
+    if(emailValue !== null){
+        let emailValues =  emailValue as string;
+        if(emailValues.split("@")[1] === "gmail.com"){
+          returnany = null;
+        }
+        else{
+          returnany = { 'isnotGmail' : true}
+        }
+    }
+    return returnany
+  }
+
+
+
+  get customerFormArray(): FormArray {
+    return this.CustomerForm.get('CustomerDetails') as FormArray;
+  }
+
+  evtAdd() {
+    let FormArray = this.CustomerForm.get('CustomerDetails') as FormArray;
+    FormArray.push(
+      new FormGroup({
+        'ContactName': new FormControl(),
+        'OrderName': new FormControl()
+      })
+    );
   }
 
   cityNames = [
@@ -36,7 +97,7 @@ export class ReactiveApproachComponent implements OnInit {
     {
       ID: 3,
       name: 'Chennai',
-    }
+    },
   ];
 
   evtSubmit() {
